29f27770aebf23715fed205b688792f240dac9e3
Lucasknutsater <lucasknutsater@gmail.com>
2017-04-06 10:32:13 +0200

    La till en ny konstruktor

--- src/serverMain/Message.java
+++ src/serverMain/Message.java
@@ -1,18 +1,22 @@
 package serverMain;
 
+import java.awt.Image;
 import java.util.Date;
 
-import javax.swing.ImageIcon;
 /**
  * Message class which contains the message being sent from and to server/client.
  * @author Lucas Knutsäter
  *
  */
 public class Message {
-	private ImageIcon image;
+	static final int LOGIN = 0, MESSAGE = 1;
+	private Image image;
 	private String sender;
 	private String recipient;
+	private String username;
+	private String password;
 	private Date date;
+	private int type;
 	
 	/**
 	 *  Constructor add all information and put date when sent.
@@ -20,17 +24,23 @@ public class Message {
 	 * @param recipient Recipient of message
 	 * @param image image to send
 	 */
-	public Message(String sender, String recipient, ImageIcon image) {
+	public Message(String sender, String recipient, Image image) {
 		this.sender = sender;
+		this.type = 1;
 		this.recipient = recipient;
 		this.image = image;
 		this.date = new Date();
+	}
+	public Message(String username, String password) {
+		this.username = username;
+		this.password = password;
+		this.type = 0;
 	}
 	/**
 	 * returns image
 	 * @return image in message
 	 */
-	public ImageIcon getImage() {
+	public Image getImage() {
 		return image;
 	}
 	/**
