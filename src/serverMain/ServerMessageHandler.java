0d702ed40319b63719ca85da83ce8b8e2ff52522
DavidSandh <kung.sandh@gmail.com>
2017-04-07 13:32:21 +0200

    Småfixar
    David & Lucas

--- src/serverMain/ServerMessageHandler.java
+++ src/serverMain/ServerMessageHandler.java
@@ -5,39 +5,47 @@ import java.util.HashMap;
 
 import message.Message;
 
+ /**
+  * ÄNDRA STRING TILL MESSAGE
+  * @author kungs
+  *
+  */
 public class ServerMessageHandler {
-	private HashMap<String, ArrayList<String>> messageBuffer = new HashMap<String, ArrayList<String>>();
+	private static HashMap<String, ArrayList<Message>> messageBuffer = new HashMap<String, ArrayList<Message>>();
 	
-	protected void put(String username, String message) {
+	
+	protected static void put(String username, Message message) {
 		if (!messageBuffer.containsKey(username)) {
-			messageBuffer.put(username, new ArrayList<String>());
-			System.out.println("nyckelfinnsinte  " + username + " : " + messageBuffer.get(username).toString());
+			messageBuffer.put(username, new ArrayList<Message>());
+			System.out.println("nyckelfinnsinte  " + username + " : " + messageBuffer.get(username).toString()); //Remove
 		}
 			messageBuffer.get(username).add(message);	
-			System.out.println("nyckelfinns "  + username + " : " + messageBuffer.get(username).toString());
+			System.out.println("nyckelfinns "  + username + " : " + messageBuffer.get(username).toString()); //remove
 	}
-	protected String remove(String username) {
-		ArrayList<String> tempList = messageBuffer.get(username);
+	protected static Message remove(String username) {
+		ArrayList<Message> tempList = messageBuffer.get(username);
 		if(!tempList.isEmpty()) {
 			return tempList.remove(0);
 		}
 		return null;
 	}
-	public static void main(String [] args) {
-		ServerMessageHandler test = new ServerMessageHandler();
-		test.put("nyckel", "value");
-		test.put("nyckel", "value1");
-		test.put("nyckel", "value2");
-		test.put("nyckel", "value3");
-		test.put("nyckel2", "value0");
-		test.put("nyckel2", "value1");
-		test.put("nyckel2", "value2");
-		String testString = test.remove("nyckel");
-		while(!(testString == null)){
-			System.out.println(testString);
-			testString = test.remove("nyckel");
-		}
-		
-		
-	}
+	/**
+	 * TA BORT SEN
+	 * @param args
+	 */
+//	public static void main(String [] args) {
+//		ServerMessageHandler test = new ServerMessageHandler();
+//		test.put("nyckel", "value");
+//		test.put("nyckel", "value1");
+//		test.put("nyckel", "value2");
+//		test.put("nyckel", "value3");
+//		test.put("nyckel2", "value0");
+//		test.put("nyckel2", "value1");
+//		test.put("nyckel2", "value2");
+//		String testString = test.remove("nyckel");
+//		while(!(testString == null)){
+//			System.out.println(testString);
+//			testString = test.remove("nyckel");
+//		}
+//	}
 }
