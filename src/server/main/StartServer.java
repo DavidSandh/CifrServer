afef3dbc3a058beb58739ac78a350e0ee24c4737
DavidSandh <kung.sandh@gmail.com>
2017-05-04 10:53:38 +0200

    d
    d

--- src/server/main/StartServer.java
+++ src/server/main/StartServer.java
@@ -1,7 +1,11 @@
 package server.main;
 
 import java.io.IOException;
-
+/**
+ * Starts the server.
+ * @author David Sandh, Lucas Knutsäter
+ *
+ */
 public class StartServer {
 	public static void main(String[] args) throws IOException {
 		ServerGUI serverGUI = new ServerGUI();
